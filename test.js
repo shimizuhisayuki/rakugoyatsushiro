375a5540769570cedd3a15a19cc0c4c122cdff95
shimizuhisayuki <rakugoyatsushiro@gmail.com>
2025-11-14 12:08:44 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8" />
+<title>JS動作確認</title>
+</head>
+<body>
+  <button onclick="alert('JavaScriptは動いています！')">動作確認ボタン</button>
+</body>
+</html>
